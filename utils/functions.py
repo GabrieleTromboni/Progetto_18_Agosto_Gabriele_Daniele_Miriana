a8d6efb2d21e8bbf7c7a6768d11344119a9bf862
miripom <37905291+miripom@users.noreply.github.com>
2025-08-18 17:24:21 +0200

    Update functions.py
    <feat>(miriana): added cerca_per_nome function

--- utils/functions.py
+++ utils/functions.py
@@ -8,3 +8,7 @@ def aggiungi_studente(lista,nome,cognome):
     
 def ordina_studenti(lista):
     return sorted(lista, key=itemgetter(1,0))
+
+def cerca_per_nome(lista, stringa):
+    studenti = [(nome,cognome) for nome, cognome in lista if stringa.lower() in nome.lower()]
+    return (True, studenti) if studenti else False
