6ac3a3e22b211fae014cd328c8f80dcd995dfca5
miripom <37905291+miripom@users.noreply.github.com>
2025-08-18 16:57:58 +0200

    Update students.py
    <feat>(gabriele): creazione dati sotto forma di liste

---
+++ utils/students.py
@@ -0,0 +1,3 @@
+listaGabriele = [("Mario","Rossi"), ("Gianluca","Rossi"), ("Gabriele","Tromboni"),("Miriana","Pompilio"),("Daniele","Mirabella")]
+listaMiriana = [("Gabriele","Tromboni"),("Miriana","Pompilio"),("Daniele","Mirabella"), ("Fabrizio","Verdi"),("Matteo","Alonzi")]
+listaDaniele = [("Gabriele","Tromboni"),("Miriana","Pompilio"),("Daniele","Mirabella"),("Marco","Rossi"),("Flavia","Rinaldi")]
