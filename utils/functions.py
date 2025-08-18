45801270757950b243b9f9c9bf2052f1cc7c8efe
miripom <37905291+miripom@users.noreply.github.com>
2025-08-18 17:12:33 +0200

    Update functions.py
    <feat>(miriana): added ordina_studenti function

--- utils/functions.py
+++ utils/functions.py
@@ -1,5 +1,10 @@
+from operator import itemgetter
+
 def aggiungi_studente(lista,nome,cognome):
     try:
         lista.append(nome,cognome)
     except Exception as e:
         raise TypeError
+    
+def ordina_studenti(lista):
+    return sorted(lista, key=itemgetter(1,0))
