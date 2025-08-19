0a2ce5726bdbf339823f1cf0a21a30e211c3fcf5
GabrieleTromboni <Gabriele.Tromboni@it.ey.com>
2025-08-19 08:43:03 +0200

    Update functions.py
    <refactor> (gabriele): Docustring complete per ogni funzione, piccoli fix allo script aggiungnedo la struttura try/except

--- utils/functions.py
+++ utils/functions.py
@@ -1,6 +1,3 @@
-from operator import itemgetter
-
-
 '''
 Questo script raccoglie le varie funzioni da richiamare nello script 'student_manager.py'.
 Abbiamo tre differenti funzioni:
@@ -8,6 +5,9 @@ Abbiamo tre differenti funzioni:
     - ordina_studenti: permette di ordinare gli studenti presenti nella lista in ordine alfabetico.
     - cerca_per_nome: 
 '''
+
+from operator import itemgetter
+from typing import List
 
 # FUNZIONI
 def aggiungi_studente(
@@ -29,14 +29,53 @@ def aggiungi_studente(
     '''
     
     try:
+        # appendere alla lista esistente la tupla (nome, cognome) dello studente
         lista.append(nome,cognome)
     except TypeError as e:
         print(f"TypeError occured: {str(e)}. All arguments must be passed to this function in str format")
 
 
-def ordina_studenti(lista):
-    return sorted(lista, key=itemgetter(1,0))
+def ordina_studenti(lista: str) -> List:
+    '''
+    Questa funzione permette di ordinare gli studenti presenti in una lista già esistente, 
+    sia prima che dopo aver aggiunto uno o più studenti alla stessa lista o a varie liste.
+    
+    Args:
+        lista (str): Nome della lista esistente da oridnare.
+        
+    Returns:
+        lista ordinata in ordine alfabetico.
+    '''
 
-def cerca_per_nome(lista, stringa):
-    studenti = [(nome,cognome) for nome, cognome in lista if stringa.lower() in nome.lower()]
-    return (True, studenti) if studenti else False
+    try:
+        # ordinare la lista di tuple dando priorità al cognome dello studente
+        return sorted(lista, key=itemgetter(1,0))
+    except TypeError as e:
+        print(f"TypeError occured: {str(e)}. All arguments must be passed to this function in str format")
+
+def cerca_per_nome(
+        lista: str, 
+        stringa: str
+    ) -> tuple:
+    '''
+    La funzione permette di cercare un qualsiasi studente nella lista esistente chiamata.
+    Se la stringa da cercare è un nome completo o incompleto, non cambia nulla
+    e avremo in output una tupla che ci indica che la ricerca è andata a buon fine (True)
+    associando la lista dei vari nomi di studenti trovati in cui la stringa è presente.
+    
+    Args:
+        lista (str): Nome della lista in cui ricercare la stringa.
+        stringa (str): stringa da ricercare nella lista esistente.
+        
+    Returns:
+        tuple or bool in base al successo o non successo della ricerca.
+    '''
+
+    try:
+        # ricerchiamo la stringa passata in ogni nome di tutti gli studenti presenti nella lista esistente richiamata
+        # lista di tuple (nome, cognome) derivanti dalla lista esistente selezionate SE la stringa è presente nel nome!
+        studenti = [(nome,cognome) for nome, cognome in lista if stringa.lower() in nome.lower()]
+        # ritorna tupla sia per il successo che per l'insuccesso della ricerca
+        return (True, studenti) if studenti else (False, [])
+    except TypeError as e:
+        print(f"TypeError occured: {str(e)}. All arguments must be passed to this function in str format")
